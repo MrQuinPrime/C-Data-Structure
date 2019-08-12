8b7c5709539bb1d7a68aca4bd462d55230dfa825
MrQuinPrime <1135623229@qq.com>
2019-08-12 12:09:58 +0800

    001
    Linear table

---
+++ 线性表/001-线形表/001-线形表/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_线形表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用我们自己的顺序表
+            SeqList<string> seqList = new SeqList<string>();
+            seqList.Add("123");
+            seqList.Add("456");
+            seqList.Add("789");
+
+
+            Console.WriteLine(seqList.GetEle(0));
+            Console.WriteLine(seqList[0]);
+            seqList.Insert("777", 1);
+            for(int i=0;i<seqList.GetLength();i++)
+            {
+                Console.Write(seqList[i] + " ");
+            }
+            Console.WriteLine();
+
+            seqList.Delete(0);
+
+            for (int i = 0; i < seqList.GetLength(); i++)
+            {
+                Console.Write(seqList[i] + " ");
+            }
+            Console.WriteLine();
+            seqList.Clear();
+            Console.WriteLine(seqList.GetLength());
+            Console.ReadKey();
+        }
+    }
+}
